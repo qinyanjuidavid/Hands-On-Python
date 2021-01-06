a74e1977444d752e6b6355cb84e317ccfa648f1d
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-01-07 00:14:26 +0300

    Hands On If Statements :octocat:

--- FlowOfControl.py
+++ FlowOfControl.py
@@ -23,34 +23,40 @@ if temp>70:
 else:
     print('I think you should wear long pants.')
 print("Get some exercise outside")
-#More Conditional Expressions
-def CalcWeeklyWages(totalHours,hourlyWage):
-    """Return the total weekly wages for a worker working totalHours with a given regular hourlywage, include overtime for hours over 40"""
-    if totalHours<=40:
-        totalwages=hourlyWage*totalHours
-    else:
-        overtime=totalHours-40
-        totalWages=hourlyWage*40+(1.5*hourlyWage)*overtime
-    return totalwages
-def Main():
-    # hours=float(input("Enter hours worked"))
-    # wage=float(input("Enter dollars paid per hour:"))
-    hours=40.0
-    wage=2.0
-    total=CalcWeeklyWages(hours,wage)
-    print(f"Wages for {hours} hours at ${round(wage,2)} per hour are ${round(total,2)}")
-Main()
-from random import randint
-class headsTail:
-    def __init__(self):
-        """:arg"""
-    def flip(self):
-        guess = input("1 or 0:")
-        comp=randint(guess)
-        guess.capitalize()
-        if guess==comp:
-            print("Congrats!, You won....")
-        else:
-            print(f"Ooopps!, You failed... The outcome is {comp}")
-day=headsTail()
-day.flip()
+# #More Conditional Expressions
+# def CalcWeeklyWages(totalHours,hourlyWage):
+#     """Return the total weekly wages for a worker working totalHours with a given regular hourlywage, include overtime for hours over 40"""
+#     if totalHours<=40:
+#         totalwages=hourlyWage*totalHours
+#     else:
+#         overtime=totalHours-40
+#         totalWages=hourlyWage*40+(1.5*hourlyWage)*overtime
+#     return totalwages
+# def Main():
+#     # hours=float(input("Enter hours worked"))
+#     # wage=float(input("Enter dollars paid per hour:"))
+#     hours=40.0
+#     wage=2.0
+#     total=CalcWeeklyWages(hours,wage)
+#     print(f"Wages for {hours} hours at ${round(wage,2)} per hour are ${round(total,2)}")
+# # Main()
+# from random import randint
+# class headsTail:
+#     def __init__(self):
+#         """:arg"""
+#     def flip(self):
+#         guess = input("1 or 0:")
+#         comp=randint(guess)
+#         guess.capitalize()
+#         if guess==comp:
+#             print("Congrats!, You won....")
+#         else:
+#             print(f"Ooopps!, You failed... The outcome is {comp}")
+# day=headsTail()
+# day.flip()
+
+def letterGrade(score):
+    score=eval(input(""))
+    if score>=80:
+        letter="A"
+
